ec3c6cd728f6c5d97e30273aa87b75d3591da545
Jarod Bernabe <jarodbernabe1100@gmail.com>
2023-04-19 20:31:30 +0800

    added build prod

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const generateId = () => {
 app.use(express.json());
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+app.use(express.static("build"));
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
